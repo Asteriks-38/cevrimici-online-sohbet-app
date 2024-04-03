45acc7366d42792ec9e06b99c20db3e477afe85f
bhantt <118824668+Baut0@users.noreply.github.com>
2024-04-03 19:08:36 +0300

    login system added
    session code soon

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/login.css">
+  <title>LoLo Login</title>
+</head>
+<body>
+  <h1>LoLo Chat</h1>
+  <div class="login">
+    <h2>Login</h2>
+    <p>For Family</p>
+    <form action="" method="post">
+
+      <h3>Username: </h3>
+      <input type="text" name="username" id="" placeholder="Username">
+
+      <h3>Password: </h3>
+      <input type="password" name="password" id="" placeholder="Password">
+
+      <button>Login/Register</button>
+
+    </form>
+  </div>
+</body>
+</html>
